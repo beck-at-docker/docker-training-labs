ab7229df58cab2d9102d4b9adc77928949e805aa
Beck <beck.matuszek@docker.com>
2026-02-25 09:20:32 -0600

    fix: remove set -e from interactive script, standardize increment style, drop stale python3 check from bootstrap

--- tests/test_framework.sh
+++ tests/test_framework.sh
@@ -19,17 +19,17 @@ TESTS_FAILED=0
 # Logging functions
 log_test() {
     echo -e "${BLUE}[TEST]${NC} $1"
-    ((TESTS_RUN++))
+    TESTS_RUN=$((TESTS_RUN + 1))
 }
 
 log_pass() {
     echo -e "${GREEN}[PASS]${NC} $1"
-    ((TESTS_PASSED++))
+    TESTS_PASSED=$((TESTS_PASSED + 1))
 }
 
 log_fail() {
     echo -e "${RED}[FAIL]${NC} $1"
-    ((TESTS_FAILED++))
+    TESTS_FAILED=$((TESTS_FAILED + 1))
 }
 
 log_warn() {
